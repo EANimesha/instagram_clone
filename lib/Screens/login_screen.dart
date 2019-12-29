5e943aeb0346fd4d7d459f2fca1dfbaca49edc71
EANimesha <nimeshadilini999@gmail.com>
2019-12-29 07:12:31 +0530

    designed the ui for both login and signup

---
+++ lib/Screens/login_screen.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone/Screens/signup_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+
+  static final String id='login_screen';
+
+  LoginScreen({Key key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey=GlobalKey<FormState>();
+  String _email,_password;
+
+  _onSubmit(){
+    if(_formKey.currentState.validate()){
+      _formKey.currentState.save();
+      print(_email);
+      print(_password);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text('Instagram',style:TextStyle(fontSize: 50.0,fontFamily: 'VeganStyle')),
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Padding(
+                     padding: EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 10.0
+                    ),
+                    child: TextFormField(
+                       decoration: InputDecoration(labelText: 'Email'),
+                       validator: (input)=>!input.contains('@')?'PLease Enter Valid email':null,
+                       onSaved:(input)=>_email=input,
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 30.0,
+                      vertical: 10.0
+                    ),
+                  
+                    child:TextFormField(
+                      decoration: InputDecoration(labelText: 'Password'),
+                      validator: (input)=>input.length<6?'Must be at least 6 characters':null,
+                      onSaved:(input)=>_password=input ,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    width: 250.0,
+                    child: FlatButton(
+                      onPressed: _onSubmit,
+                      padding: EdgeInsets.all(10.0),
+                      color: Colors.blue,
+                      child: Text('Login',style:TextStyle(color:Colors.white,fontSize: 18.0)),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  Container(
+                    width: 250.0,
+                    child: FlatButton(
+                      onPressed:()=>Navigator.pushNamed(context, SignUpScreen.id),
+                      padding: EdgeInsets.all(10.0),
+                      color: Colors.blue,
+                      child: Text('Go to SignUp',style:TextStyle(color:Colors.white,fontSize: 18.0)),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/signup_screen.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatefulWidget {
+  static final String id='sign_up_screen';
+  SignUpScreen({Key key}) : super(key: key);
+
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+  final _formKey=GlobalKey<FormState>();
+  String _email,_password,_name;
+
+  _onSubmit(){
+    if(_formKey.currentState.validate()){
+      _formKey.currentState.save();
+      print(_email);
+      print(_password);
+      print(_name);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text('Instagram',style:TextStyle(fontSize: 50.0,fontFamily: 'VeganStyle')),
+              Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Padding(
+                       padding: EdgeInsets.symmetric(
+                        horizontal: 30.0,
+                        vertical: 5.0
+                      ),
+                      child: TextFormField(
+                         decoration: InputDecoration(labelText: 'Name'),
+                         validator: (input)=>input.trim().isEmpty?'PLease Enter Valid name':null,
+                         onSaved:(input)=>_name=input,
+                      ),
+                    ),
+                    Padding(
+                       padding: EdgeInsets.symmetric(
+                        horizontal: 30.0,
+                        vertical: 5.0
+                      ),
+                      child: TextFormField(
+                         decoration: InputDecoration(labelText: 'Email'),
+                         validator: (input)=>!input.contains('@')?'PLease Enter Valid email':null,
+                         onSaved:(input)=>_email=input,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 30.0,
+                        vertical: 5.0
+                      ),
+                    
+                      child:TextFormField(
+                        decoration: InputDecoration(labelText: 'Password'),
+                        validator: (input)=>input.length<6?'Must be at least 6 characters':null,
+                        onSaved:(input)=>_password=input ,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed: _onSubmit,
+                        padding: EdgeInsets.all(10.0),
+                        color: Colors.blue,
+                        child: Text('SignUp',style:TextStyle(color:Colors.white,fontSize: 18.0)),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Container(
+                      width: 250.0,
+                      child: FlatButton(
+                        onPressed:()=>Navigator.pop(context),
+                        padding: EdgeInsets.all(10.0),
+                        color: Colors.blue,
+                        child: Text('Back to Login',style:TextStyle(color:Colors.white,fontSize: 18.0)),
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
